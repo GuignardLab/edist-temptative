bcec8c2115f8f04d22fecc3985c4e4f9d09b3e6d
Benjamin Paassen <bpaassen@techfak.uni-bielefeld.de>
2019-05-18 15:32:51 +0200

    refactored python parallelization in external file and added some utility function for even faster DTW computation for specific standard choices of distance (manhattan, Euclidean, string)

--- dtw.pyx
+++ dtw.pyx
@@ -1,45 +1,7 @@
 import numpy as np
 from cython.parallel import prange
-import multiprocessing as mp
-
-def idx_producer(const int m, const int n):
-	cdef int i
-	cdef int j
-	for i in range(m):
-		for j in range(n):
-			yield (i,j)
-
-def dtw_with_idxs(x, y, delta, k, l):
-	return (k, l, dtw(x, y, delta))
-
-def multi_dtw(list Xs, list Ys, delta, int num_jobs = 8):
-	# perform dynamic time warping on all pairwise sequence combinations
-	cdef int K = len(Xs)
-	cdef int L = len(Ys)
-	# set up a parallel processing pool
-	pool = mp.Pool(num_jobs)
-	# set up the result matrix
-	D = np.zeros((K,L))
-	cdef double[:,:] D_view = D
-
-	# set up the dtw index function
-	# and the callback function
-	def callback(tpl):
-		D_view[tpl[0], tpl[1]] = tpl[2]
-
-	# start off all parallel processing jobs
-	for k in range(K):
-		for l in range(L):
-			pool.apply_async(dtw_with_idxs, args=(Xs[k], Ys[l], delta, k, l), callback=callback)
-
-	# wait for the jobs to finish
-	pool.close()
-	pool.join()
-
-	return D
-
-def delta_with_idxs(x, y, delta, i, j):
-	return (i, j, delta(x, y))
+from libc.math cimport sqrt
+cimport cython
 
 def dtw(x, y, delta):
 	cdef int m = len(x)
@@ -58,6 +20,103 @@ def dtw(x, y, delta):
 	D = np.zeros((m,n))
 	return dtw_c(Delta, D)
 
+@cython.boundscheck(False)
+def dtw_numeric(double[:] x, double[:] y):
+	cdef int m = len(x)
+	cdef int n = len(y)
+	# First, compute all pairwise replacements
+	# using OMP parallelization
+	cdef int i
+	cdef int j
+	Delta = np.zeros((m, n))
+	cdef double[:,:] Delta_view = Delta
+	for i in prange(m, nogil=True):
+		for j in prange(n):
+			if(x[i] > y[j]):
+				Delta_view[i,j] = x[i] - y[j]
+			else:
+				Delta_view[i,j] = y[j] - x[i]
+	# Then, compute the dynamic time warping
+	# distance
+	D = np.zeros((m,n))
+	return dtw_c(Delta, D)
+
+@cython.boundscheck(False)
+def dtw_manhattan(double[:,:] x, double[:,:] y):
+	cdef int m = x.shape[0]
+	cdef int n = y.shape[0]
+	cdef int K = x.shape[1]
+	if(y.shape[1] != K):
+		raise ValueError('x and y do not have the same dimensionality (%d versus %d)' % (x.shape[1], y.shape[1]))
+	# First, compute all pairwise replacements
+	# using OMP parallelization
+	cdef int i
+	cdef int j
+	Delta = np.zeros((m, n))
+	cdef double[:,:] Delta_view = Delta
+	cdef int k
+	cdef double diff
+	for i in prange(m, nogil=True):
+		for j in prange(n):
+			for k in prange(K):
+				diff = x[i,k] - y[j,k]
+				if(diff < 0):
+					Delta_view[i, j] -= diff
+				else:
+					Delta_view[i, j] += diff
+	# Then, compute the dynamic time warping
+	# distance
+	D = np.zeros((m,n))
+	return dtw_c(Delta, D)
+
+@cython.boundscheck(False)
+def dtw_euclidean(double[:,:] x, double[:,:] y):
+	cdef int m = x.shape[0]
+	cdef int n = y.shape[0]
+	cdef int K = x.shape[1]
+	if(y.shape[1] != K):
+		raise ValueError('x and y do not have the same dimensionality (%d versus %d)' % (x.shape[1], y.shape[1]))
+	# First, compute all pairwise replacements
+	# using OMP parallelization
+	cdef int i
+	cdef int j
+	Delta = np.zeros((m, n))
+	cdef double[:,:] Delta_view = Delta
+	cdef int k
+	cdef double diff
+	for i in prange(m, nogil=True):
+		for j in prange(n):
+			for k in prange(K):
+				diff = x[i,k] - y[j,k]
+				Delta_view[i, j] += diff * diff
+			Delta_view[i, j] = sqrt(Delta_view[i, j])
+	# Then, compute the dynamic time warping
+	# distance
+	D = np.zeros((m,n))
+	return dtw_c(Delta, D)
+
+@cython.boundscheck(False)
+def dtw_string(str x, str y):
+	cdef int m = len(x)
+	cdef int n = len(y)
+	# First, compute all pairwise replacements
+	# using OMP parallelization
+	cdef int i
+	cdef int j
+	Delta = np.zeros((m, n))
+	cdef double[:,:] Delta_view = Delta
+	for i in range(m):
+		for j in range(n):
+			if(x[i] == y[j]):
+				Delta_view[i, j] = 0.
+			else:
+				Delta_view[i, j] = 1.
+	# Then, compute the dynamic time warping
+	# distance
+	D = np.zeros((m,n))
+	return dtw_c(Delta, D)
+
+@cython.boundscheck(False)
 cdef double dtw_c(const double[:,:] Delta, double[:,:] D) nogil:
 	cdef int i
 	cdef int j
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+#import os
 
+#os.environ['CFLAGS'] = '-O3 -Wall'
 setup(name='DTW app', ext_modules=cythonize("dtw.pyx"), zip_safe=False)
