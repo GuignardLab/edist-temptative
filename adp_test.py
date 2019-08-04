4da11ca785ab9589136f3c50b94ddf1e1ab9908b
Benjamin Paassen <bpaassen@techfak.uni-bielefeld.de>
2019-08-04 22:07:13 +0200

    implemented ADP backtracing

--- adp_test.py
+++ adp_test.py
@@ -1,28 +1,92 @@
 import unittest
-import edist.grammar as grammar
+from edist.alignment import Alignment
+from edist.adp import Grammar
 from edist.adp import edit_distance
+from edist.adp import backtrace
 
 def kron_distance(x, y):
-	if(x == y):
-		return 0.
-	else:
-		return 1.
+    if(x == y):
+        return 0.
+    else:
+        return 1.
 
 class TestGrammarMethods(unittest.TestCase):
 
-	def test_edit_distance(self):
-		gra = grammar.Grammar('A', ['A'])
-		gra.append_replacement('A', 'A', 'rep')
-		gra.append_deletion('A', 'A', 'del')
-		gra.append_insertion('A', 'A', 'ins')
-		left = ['a', 'b', 'c']
-		right = ['a', 'd', 'e', 'c']
-		deltas = {'rep' : kron_distance, 'del' : kron_distance, 'ins' : kron_distance}
-		expected = 2.
-		actual = edit_distance(left, right, gra, deltas)
-		self.assertEqual(expected, actual)
-		actual = edit_distance(left, right, gra, kron_distance)
-		self.assertEqual(expected, actual)
+    def test_edit_distance(self):
+        gra = Grammar('A', ['A'])
+        gra.append_replacement('A', 'A', 'rep')
+        gra.append_deletion('A', 'A', 'del')
+        gra.append_insertion('A', 'A', 'ins')
+        left = ['a', 'b', 'c']
+        right = ['a', 'd', 'e', 'f', 'c']
+        deltas = {'rep' : kron_distance, 'del' : kron_distance, 'ins' : kron_distance}
+        expected = 3.
+        actual = edit_distance(left, right, gra, deltas)
+        self.assertEqual(expected, actual)
+        actual = edit_distance(left, right, gra, kron_distance)
+        self.assertEqual(expected, actual)
+
+        # set up a different grammar and process the same two strings again
+        skip_gra = Grammar('A', ['A', 'Sk'])
+        skip_gra.append_replacement('A', 'A', 'rep')
+        skip_gra.append_deletion('A', 'Sk', 'del')
+        skip_gra.append_insertion('A', 'Sk', 'ins')
+        skip_gra.append_replacement('Sk', 'A', 'rep')
+        skip_gra.append_deletion('Sk', 'Sk', 'skdel')
+        skip_gra.append_insertion('Sk', 'Sk', 'skins')
+
+        def skcost(x, y):
+            return 0.5
+        deltas = {'rep' : kron_distance, 'del' : kron_distance, 'ins' : kron_distance, 'skdel' : skcost, 'skins' : skcost}
+
+        expected = 2.5
+        actual = edit_distance(left, right, skip_gra, deltas)
+        self.assertEqual(expected, actual)
+
+    def test_backtrace(self):
+        gra = Grammar('A', ['A'])
+        gra.append_replacement('A', 'A', 'rep')
+        gra.append_deletion('A', 'A', 'del')
+        gra.append_insertion('A', 'A', 'ins')
+        left = ['a', 'b', 'c']
+        right = ['a', 'd', 'e', 'f', 'c']
+
+        # set up expected alignment
+        expected_ali = Alignment()
+        expected_ali.append_tuple(0, 0, 'rep')
+        expected_ali.append_tuple(1, 1, 'rep')
+        expected_ali.append_tuple(-1, 2, 'ins')
+        expected_ali.append_tuple(-1, 3, 'ins')
+        expected_ali.append_tuple(2, 4, 'rep')
+
+        # compare to actual alignment
+        actual_ali = backtrace(left, right, gra, kron_distance)
+        self.assertEqual(expected_ali, actual_ali)
+
+        # set up a different grammar and process the same two strings again
+        skip_gra = Grammar('A', ['A', 'Sk'])
+        skip_gra.append_replacement('A', 'A', 'rep')
+        skip_gra.append_deletion('A', 'Sk', 'del')
+        skip_gra.append_insertion('A', 'Sk', 'ins')
+        skip_gra.append_replacement('Sk', 'A', 'rep')
+        skip_gra.append_deletion('Sk', 'Sk', 'skdel')
+        skip_gra.append_insertion('Sk', 'Sk', 'skins')
+
+        def skcost(x, y):
+            return 0.5
+        deltas = {'rep' : kron_distance, 'del' : kron_distance, 'ins' : kron_distance, 'skdel' : skcost, 'skins' : skcost}
+
+        # set up expected alignment
+        expected_ali = Alignment()
+        expected_ali.append_tuple(0, 0, 'rep')
+        expected_ali.append_tuple(1, 1, 'rep')
+        expected_ali.append_tuple(-1, 2, 'ins')
+        expected_ali.append_tuple(-1, 3, 'skins')
+        expected_ali.append_tuple(2, 4, 'rep')
+
+        # compare to actual alignment
+        actual_ali = backtrace(left, right, skip_gra, deltas)
+        self.assertEqual(expected_ali, actual_ali)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- edist/adp.pyx
+++ edist/adp.pyx
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from collections.abc import Callable
 import numpy as np
+from edist.alignment import Alignment
 
 __author__ = 'Benjamin Paaßen'
 __copyright__ = 'Copyright 2019, Benjamin Paaßen'
@@ -452,3 +453,199 @@ def edit_distance(x, y, grammar, deltas):
 
     # return the cost at the start symbol
     return Ds[start_idx, 0, 0]
+
+####### BACKTRACING FUNCTIONS #######
+
+cdef double _BACKTRACE_TOL = 1E-5
+
+def backtrace(x, y, grammar, deltas):
+    """ Computes a co-optimal alignment between the two input sequences
+    x and y, given the given ADP grammar and algebra. This mechanism
+    is deterministic and will always prefer replacements over other options.
+
+    Args:
+    x:       a sequence of objects.
+    y:       another sequence of objects.
+    grammar: An ADP grammar. Refer to the documentation above for more
+             information.
+    deltas:  An algebra, i.e. a mapping from operation names to distance
+             functions OR a single distance function if the grammar supports
+             only a single replacement, deletion, and insertion operation.
+
+    Returns: a co-optimal alignment.Alignment between x and y.
+    """
+    # check if the given algebra is compatible with the given grammar.
+    if(isinstance(deltas, Callable)):
+        if(len(grammar._reps) > 1 or len(grammar._dels) > 1 or len(grammar._inss) > 1):
+            raise ValueError('If a function is given instead of an algebra, the grammar can only support a single operation of each type; otherwise, ambiguities arise.')
+        # generate a mock algebra that is definitely compatible
+        delta = deltas
+        deltas = {grammar._reps[0] : delta, grammar._dels[0] : delta, grammar._inss[0] : delta}
+    else:
+        grammar.validate(deltas)
+
+    cdef int m = len(x)
+    cdef int n = len(y)
+    # pre-compute all operation costs
+
+    # First, compute all pairwise replacements
+    cdef int K_rep = len(grammar._reps)
+    Deltas_rep = np.zeros((K_rep, m, n))
+    cdef double[:,:,:] Deltas_rep_view = Deltas_rep
+    cdef int i
+    cdef int j
+    cdef int k
+    for k in range(K_rep):
+        delta = deltas[grammar._reps[k]]
+        for i in range(m):
+            for j in range(n):
+                Deltas_rep_view[k, i, j] = delta(x[i], y[j])
+
+    # Then, compute all deletions
+    cdef int K_del = len(grammar._dels)
+    Deltas_del = np.zeros((K_del, m))
+    cdef double[:,:] Deltas_del_view = Deltas_del
+    for k in range(K_del):
+        delta = deltas[grammar._dels[k]]
+        for i in range(m):
+            Deltas_del_view[k, i] = delta(x[i], None)
+
+    # Then, compute all insertions
+    cdef int K_ins = len(grammar._inss)
+    Deltas_ins = np.zeros((K_ins, n))
+    cdef double[:,:] Deltas_ins_view = Deltas_ins
+    for k in range(K_ins):
+        delta = deltas[grammar._inss[k]]
+        for j in range(n):
+            Deltas_ins_view[k, j] = delta(None, y[j])
+
+    # retrieve the adjacency list representation for
+    # the grammar
+    start_idx, accpt_idxs, adj_rep, adj_del, adj_ins = grammar.adjacency_lists()
+
+    # Initialize the dynamic programming matrices
+    # for all nonterminals
+    cdef int R = len(grammar._nonterminals)
+    Ds = np.full((R, m+1, n+1), np.inf)
+    cdef double[:,:,:] Ds_view = Ds
+    # initialize last entry for all accepting symbols
+    cdef int nont
+    for nont in accpt_idxs:
+        Ds_view[nont, m, n] = 0.
+
+    # initialize last column for all symbols
+    cdef int r
+    cdef int s
+    for i in range(m-1,-1,-1):
+        for r in range(R):
+            for (k, s) in adj_del[r]:
+                Ds_view[r, i, n] = Deltas_del_view[k, i] + Ds_view[s, i+1, n]
+
+    # initialize last row for all symbols
+    for j in range(n-1,-1,-1):
+        for r in range(R):
+            for (k, s) in adj_ins[r]:
+                Ds_view[r, m, j] = Deltas_ins_view[k, j] + Ds_view[s, m, j+1]
+
+    # perform the remaining computation
+    cdef double min_cost
+    cdef double current_cost
+    for i in range(m-1,-1,-1):
+        for j in range(n-1,-1,-1):
+            for r in range(R):
+                min_cost = np.inf
+                # first, consider replacements
+                for (k, s) in adj_rep[r]:
+                    current_cost = Deltas_rep_view[k, i, j] + Ds_view[s, i+1, j+1]
+                    if(current_cost < min_cost):
+                        min_cost = current_cost
+                # then, consider deletions
+                for (k, s) in adj_del[r]:
+                    current_cost = Deltas_del_view[k, i] + Ds_view[s, i+1, j]
+                    if(current_cost < min_cost):
+                        min_cost = current_cost
+                # finally, consider insertions
+                for (k, s) in adj_ins[r]:
+                    current_cost = Deltas_ins_view[k, j] + Ds_view[s, i, j+1]
+                    if(current_cost < min_cost):
+                        min_cost = current_cost
+                # set new entry to minimum
+                Ds_view[r, i, j] = min_cost
+
+    # then, perform the backtracing
+    r = start_idx
+    i = 0
+    j = 0
+    cdef int found_coopt
+    alignment = Alignment()
+    while(i < m and j < n):
+        # check which alignment option is co-optimal
+        found_coopt = False
+        # check all possible replacements
+        for (k, s) in adj_rep[r]:
+            current_cost = Deltas_rep_view[k, i, j] + Ds_view[s, i+1, j+1]
+            if(Ds_view[r, i, j] + _BACKTRACE_TOL > current_cost):
+                # replacement is co-optimal
+                alignment.append_tuple(i, j, grammar._reps[k])
+                r = s
+                i += 1
+                j += 1
+                found_coopt = True
+                break
+        if(found_coopt):
+            continue
+        # check all possible deletions
+        for (k, s) in adj_del[r]:
+            current_cost = Deltas_del_view[k, i] + Ds_view[s, i+1, j]
+            if(Ds_view[r, i, j] + _BACKTRACE_TOL > current_cost):
+                # deletion is co-optimal
+                alignment.append_tuple(i, -1, grammar._dels[k])
+                r = s
+                i += 1
+                found_coopt = True
+                break
+        if(found_coopt):
+            continue
+        # check all possible insertions
+        for (k, s) in adj_ins[r]:
+            current_cost = Deltas_ins_view[k, j] + Ds_view[s, i, j+1]
+            if(Ds_view[r, i, j] + _BACKTRACE_TOL > current_cost):
+                # insertion is co-optimal
+                alignment.append_tuple(-1, j, grammar._inss[k])
+                r = s
+                j += 1
+                found_coopt = True
+                break
+        if(not found_coopt):
+            raise ValueError('Internal error: No option is co-optimal.')
+    # delete all demaining nodes
+    while(i < m):
+        found_coopt = False
+        # check all possible deletions
+        for (k, s) in adj_del[r]:
+            current_cost = Deltas_del_view[k, i] + Ds_view[s, i+1, j]
+            if(Ds_view[r, i, j] + _BACKTRACE_TOL > current_cost):
+                # deletion is co-optimal
+                alignment.append_tuple(i, -1, grammar._dels[k])
+                r = s
+                i += 1
+                found_coopt = True
+                break
+        if(not found_coopt):
+            raise ValueError('Internal error: No option is co-optimal.')
+    # insert all demaining nodes
+    while(j < n):
+        found_coopt = False
+        # check all possible deletions
+        for (k, s) in adj_ins[r]:
+            current_cost = Deltas_ins_view[k, j] + Ds_view[s, i, j+1]
+            if(Ds_view[r, i, j] + _BACKTRACE_TOL > current_cost):
+                # insertion is co-optimal
+                alignment.append_tuple(-1, j, grammar._inss[k])
+                r = s
+                j += 1
+                found_coopt = True
+                break
+        if(not found_coopt):
+            raise ValueError('Internal error: No option is co-optimal.')
+    return alignment
