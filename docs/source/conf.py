3fea2d02d99dbef764a6d740ae0e43f05261511c
Benjamin Paassen <benjamin.paassen@sydney.edu.au>
2020-04-24 16:31:58 +0200

    added path to master doc and tried removing sys options in doc conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -20,6 +20,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'edist'
 copyright = '2019-2020, Benjamin Paassen'
 author = 'Benjamin Paassen'
+
+master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
 release = '1.1.0'
